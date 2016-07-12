179e45983fec7d4fdc3e90de05eb898a76895ce7
Nicholas Primiano <njp280@nyu.edu>
2016-07-12 13:37:10 -0400

    no message

---
+++ Assets/Scripts/Turn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turn : MonoBehaviour
+{
+	
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		bool isCrouching = false;
+		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer> (); 
+		if (Input.GetKeyDown (KeyCode.C) && !isCrouching) {
+			sr.transform.Rotate (Vector3.forward * 90);
+			isCrouching = true;
+		}
+		if (Input.GetKeyUp (KeyCode.C)) {
+			sr.transform.Rotate (Vector3.forward * -90);	
+		}
+	}
+}
